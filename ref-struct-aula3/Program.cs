c25fc781510e180ea9564ef944d32dbd8ddb6323
Zeld2 <theudelci@gmail.com>
2022-05-12 15:00:50 -0300

    add aula3

---
+++ ref-struct-aula3/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Matheus";
+p1.Idade = 24;
+p1.EnderecoPessoa = new Endereco()
+{
+Logradouro= "Rua Teste",
+Numero= 100,
+Cidade= "Bauru",
+CEP= "000000"
+};
+
+WriteLine("Fim");
